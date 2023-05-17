fb264e9c8097861f45487865fc9cb5a24905b339
kmhbDOes <kmhbipul@gmail.com>
2023-05-18 00:51:13 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-500">Hello</h1>
+      <p className="bg-purple-400">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+        ea!
+      </p>
+    </div>
+  );
+};
+
+export default Main;
